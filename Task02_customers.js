55272c85142abbd904332edb7295fcf0016238df
Eduardo Marval <emarval@and.digital>
2021-09-09 23:12:34 +0100

    Solution - Completed createCustomerDraft and createCustomer function in customer.js - Modified the customerSampleData in Task02_customers.js to have a meaningful customer - commented all the functions except fro createCustomer on Task02_customers.js

--- Task02_customers.js
+++ Task02_customers.js
@@ -8,19 +8,19 @@ const {
 const { log } = require("./logger.js");
 
 const customerSampleData = {
-  firstName: "test2",
-  lastName: "test",
-  email: "test2@test.com",
+  firstName: "John",
+  lastName: "Wick",
+  email: "jhon@wick.com",
   password: "123",
-  key: "test1233",
+  key: "jhon_wick_123",
   countryCode: "DE",
 };
 
-//createCustomer(customerSampleData).then(log).catch(log);
+createCustomer(customerSampleData).then(log).catch(log);
 
 //getCustomerByKey('test123').then(log).catch(log);
 
-getCustomerById("10cb16bf-a5d8-4f47-b664-fe5cae2f75d0").then(log).catch(log);
+// getCustomerById("10cb16bf-a5d8-4f47-b664-fe5cae2f75d0").then(log).catch(log);
 
 //createCustomerKeyVerfiedEmail(customerSampleData).then(log).catch(log);
 
--- handson/customer.js
+++ handson/customer.js
@@ -1,18 +1,50 @@
 const { apiRoot, projectKey } = require("./client.js");
 
-module.exports.getCustomerById = (ID) =>{}
+module.exports.getCustomerById = (ID) => {};
 
-module.exports.getCustomerByKey = (key) =>{}
+module.exports.getCustomerByKey = (key) => {};
 
-const createCustomerDraft = (customerData) => {}
+const createCustomerDraft = (customerData) => {
+  const { firstName, lastName, email, password, key, countryCode } =
+    customerData;
 
-module.exports.createCustomer = (customerData) =>{}
+  const customerDraft = {
+    firstName,
+    lastName,
+    email,
+    password,
+    key,
+    countryCode,
+    addresses: [
+      {
+        country: countryCode,
+      },
+    ],
+    defaultShippingAddress: 0,
+    defaultBillingAddress: 0,
+  };
+  return customerDraft;
+};
+
+module.exports.createCustomer = async (customerData) => {
+  const customerDraft = createCustomerDraft(customerData);
+
+  return apiRoot
+    .withProjectKey({ projectKey })
+    .customers()
+    .post({
+      body: {
+        ...customerDraft,
+      },
+    })
+    .execute();
+};
 
 const createCustomerDraftKey = (customerData) => {};
 
-module.exports.createCustomerKeyVerfiedEmail = (customerData) =>{}
+module.exports.createCustomerKeyVerfiedEmail = (customerData) => {};
 
 module.exports.assignCustomerToCustomerGroup = (
   customerKey,
   customerGroupKey
-) => {}
+) => {};
